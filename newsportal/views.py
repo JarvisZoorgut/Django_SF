16fba81665af03bc49013c5eaaec0f6cf54fa1f8
Ivan Grechkan <jarvis.zoorgut@gmail.com>
2024-01-04 16:41:29 +0500

    2024.01.04

--- newsportal/views.py
+++ newsportal/views.py
@@ -8,11 +8,11 @@ from newsportal.models import Post
 class PostList(ListView):
     model = Post
     ordering = 'dateCreation'
-    template_name = 'newsportal/news.html'
-    context_object_name = 'news'
+    template_name = 'newsportal/posts.html'
+    context_object_name = 'posts'
 
 
 class PostDetail(DetailView):
     model = Post
-    template_name = 'newsportal/new.html'
-    context_object_name = 'new'
+    template_name = 'newsportal/post.html'
+    context_object_name = 'post'
