1269bf4d4c25e035188c2d7401606049cdb546e0
Ivan Grechkan <jarvis.zoorgut@gmail.com>
2024-01-05 00:22:31 +0500

    2024.01.05

--- store/models.py
+++ store/models.py
@@ -4,22 +4,33 @@ from django.core.validators import MinValueValidator
 
 # Товар для нашей витрины 
 class Product(models.Model):
-    name = models.CharField(max_length=50, unique=True) # названия товаров не должны повторяться
-    description = models.TextField()
-    quantity = models.IntegerField(validators=[MinValueValidator(0)])
     # поле категории будет ссылаться на модель категории
     # все продукты в категории будут доступны через поле products
-    category = models.ForeignKey(to='Category', on_delete=models.CASCADE, related_name='products')
-    price = models.FloatField(validators=[MinValueValidator(0.0)])
+    category = models.ForeignKey(to='Category', blank=True, null=True, on_delete=models.SET_NULL, related_name='products', verbose_name='Категория')
+    
+    name = models.CharField(max_length=50, unique=True, verbose_name='Название') # названия товаров не должны повторяться
+    description = models.TextField(verbose_name='Описание')
+    quantity = models.IntegerField(validators=[MinValueValidator(0)], verbose_name='Количество')
+    price = models.FloatField(validators=[MinValueValidator(0.0)], verbose_name='Цена')
 
     def __str__(self):
-        return f'{self.name.title()}: {self.description[:20]}. Цена: {str(self.price)}'
+        return f'{self.name} : {self.description[:20]}. Цена: {str(self.price)}'
+    
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товар'
+        ordering = ['-price']
 
 
 # Категория, к которой будет привязываться товар
 class Category(models.Model):
     # названия категорий тоже не должны повторяться
-    name = models.CharField(max_length=100, unique=True) 
+    name = models.CharField(max_length=100, null=True, unique=True, verbose_name='Название') 
 
     def __str__(self):
-        return self.name.title()
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
