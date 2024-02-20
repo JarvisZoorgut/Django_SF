516d160ffaa937dd58ba1b27f2f7f85cc9d585a0
Ivan <jarvis.zoorgut@gmail.com>
2024-02-20 21:11:19 +0500

    2024.02.20

--- newsportal/management/commands/weekly_news.py
+++ newsportal/management/commands/weekly_news.py
@@ -11,8 +11,22 @@ from django_apscheduler.models import DjangoJobExecution
 
 from newsportal.models import Subscriber, Post
 
-
+# Создание логгера
 logger = logging.getLogger(__name__)
+
+# Настройка уровня логирования
+logger.setLevel(logging.INFO)
+
+# Создание обработчика для вывода логов в консоль
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# Создание форматтера для задания формата вывода логов
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+
+# Добавление обработчика к логгеру
+logger.addHandler(console_handler)
 
 
 def send_weekly_articles():
@@ -42,12 +56,14 @@ def send_weekly_articles():
             # Генерируем текст сообщения с новыми статьями
             subject = f'Контент за неделю в категории "{category}"'
             text_content = "Новый контент:\n\n"
+            html_content = "Новый контент:\n\n"
             for article in new_articles:
-                text_content += f'<a href="http://127.0.0.1:8000{article.get_absolute_url()}">{article.title}</a>'
-
+                text_content += (f'<br><a href="http://127.0.0.1:8000{article.get_absolute_url()}">{article.title} </a>- {article.text[:50]}...')
+                html_content += (f'<br><a href="http://127.0.0.1:8000{article.get_absolute_url()}">{article.title} </a>- {article.text[:50]}...')
             # Отправляем сообщение подписчику
             email = subscriber.user.email
             msg = EmailMultiAlternatives(subject, text_content, None, [email])
+            msg.attach_alternative(html_content, "text/html")
             msg.send()
 
     logger.info("Weekly articles sent successfully!")
@@ -67,7 +83,7 @@ class Command(BaseCommand):
         # Добавляем задачу отправки статей каждую пятницу в 18:00
         scheduler.add_job(
             send_weekly_articles,
-            trigger=CronTrigger(day_of_week="mon", hour="18", minute="18"),
+            trigger=CronTrigger(day_of_week="fri", hour="18", minute="00"),
             id="send_weekly_articles",
             max_instances=1,
             replace_existing=True,
